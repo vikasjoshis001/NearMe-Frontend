caf8758186fa0295d59a459d89a76e083604c7a1
vikasjoshis001 <vikasjoshis001@gmail.com>
2021-11-05 17:48:50 +0530

    Progress bar added

--- src/app/shopform/shopform.component.ts
+++ src/app/shopform/shopform.component.ts
@@ -9,19 +9,34 @@ export class ShopformComponent implements OnInit {
 
   shopform = true;
   ownerform = false;
-  val;
+  shopdata = {};
+  ownerdata = {};
+  totaldata = {};
+  step1;
+  step2;
+  step3;
   constructor() { }
 
-  addShop(data){
-    this.val = data;
+  addShop(data) {
+    this.shopdata = data.value;
+    console.log(this.shopdata)
     this.shopform = false;
     this.ownerform = true;
+    this.step1 = "active"
   }
 
-  addOwner(data){
-    this.val = data;
+  addOwner(data) {
+    this.ownerdata = data.value;
+    console.log(this.ownerdata)
+
+    this.totaldata = {
+      ...this.shopdata,
+      ...this.ownerdata
+    };
+    console.log(this.totaldata)
     this.shopform = true;
     this.ownerform = false;
+    this.step2 = "active"
   }
 
   ngOnInit(): void {
